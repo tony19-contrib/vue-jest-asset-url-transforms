236b90c6d9a4f0dd9bfae38e18c91fedce29d115
Tony Trinh <tony19@gmail.com>
2021-07-24 17:11:23 -0500

    update jest url transform to match webpack url-loader

--- tests/jest-url-transform.js
+++ tests/jest-url-transform.js
@@ -1,5 +1,25 @@
+const urlLoader = require('url-loader')
+
 module.exports = {
   process(src, filename) {
-    return `module.exports = "${filename}"`
+    const urlLoaderOptions = {
+      esModule: false,
+      limit: 4096,
+      fallback: {
+        loader: 'file-loader',
+        options: {
+          esModule: false,
+          emitFile: false,
+          name: filename,
+        },
+      },
+    }
+    const results = urlLoader.call({
+      query: urlLoaderOptions,
+      resourcePath: filename,
+    }, src)
+
+    // strip leading Webpack prefix from file path if it exists
+    return results.replace(/^module.exports = __webpack_public_path__ \+ /, 'module.exports = ')
   }
 }
